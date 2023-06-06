0a5acd2bce2cffcdae32f98521543f3f19a5dfa5
Yutaro Kijima <eddix.ibanez@outlook.jp>
2023-06-07 02:50:54 +0900

    twitter:iamge to http

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="https://sobabase.vercel.app/image.jpg" />
+        <meta name="twitter:image" content="http://sobabase.vercel.app/image.jpg" />
         <meta name="twitter:title" content="Sobabase" />
         <meta name="twitter:description" content="My Supabase Sandbox" />
         <meta property="og:image" content="https://sobabase.vercel.app/favicon.ico" />
